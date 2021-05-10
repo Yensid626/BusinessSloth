de42f1fcee005342796089136f813dfa2b39f93a
Yensid626 <43460104+Yensid626@users.noreply.github.com>
2021-05-10 01:30:50 -0700

    Return To Main Menu After Game End
    +Fixed bug preventing you from leaving GameOver page

---
+++ BusinessSloth/Assets/GameOver_ReturnToMainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameOver_ReturnToMainMenu : MonoBehaviour
+{
+
+
+
+    public void LoadMenu()
+    {
+        //Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+        //SceneManager.LoadScene("Main Menu");
+    }
+
+}
+
